a92afda1cd991d40c46a6eb0d1d60c38822a5429
anh1banan1 <72141667+anh1banan1@users.noreply.github.com>
2021-04-16 23:30:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ app.post('/users', [
         // Check validation object for errors
         let errors = validationResult(req);
 
-        if (!error.isEmpty()) {
+        if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         }
         
